b596ab75b754857ac4e75a3c1d78b7f3382267b6
yamil-90 <yamil.coloma90@gmail.com>
2022-08-08 17:50:59 -0300

    finished bowling testing

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -4,8 +4,70 @@ namespace App;
 
 class BowlingGame
 {
+    protected array $rolls = [];
+
+    const FRAMES_PER_GAME = 10;
+
     public function play(int $pins)
     {
         $this->rolls[] = $pins;
+    }
+
+    public function score()
+    {
+        $score = 0;
+        $roll = 0;
+
+        foreach (range(1, self::FRAMES_PER_GAME) as $frame) {
+
+            if ($this->isStrike($roll)) {
+                $score += $this->pinCount($roll) + $this->strikeBonus($roll);
+
+                $roll += 1;
+
+                continue;
+            }
+
+            $score += $this->defaultScore($roll);
+            
+            if ($this->isSpare($roll)) {
+                $score +=$this->spareBonus($roll);
+                
+            }
+
+            $roll += 2;
+
+            
+        }
+        return $score;
+    }
+    private function isStrike(int $roll): int
+    {
+        return $this->pinCount($roll) === 10;
+    }
+
+    private function isSpare(int $roll): int
+    {
+        return $this->defaultScore($roll) === 10;
+    }
+
+    private function strikeBonus(int $roll): int
+    {
+        return $this->defaultScore($roll + 1);
+    }
+
+    private function spareBonus(int $roll): int
+    {
+        return  $this->pinCount($roll + 2);
+    }
+
+    private function defaultScore(int $roll): int
+    {
+        return $this->pinCount($roll) + $this->pinCount($roll + 1);
+    }
+
+    protected function pinCount(int $roll): int
+    {
+        return $this->rolls[$roll];
     }
 }
--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\BowlingGame;
 use PHPUnit\Framework\TestCase;
 
 class BowlingGameTest extends TestCase
@@ -7,7 +8,85 @@ class BowlingGameTest extends TestCase
     /** @test */
     public function first_game()
     {
-        $this->assertEquals(0 , BowlingGame::play(0));
+        $bowlingGame = new BowlingGame;
+
+        for ($i = 0; $i < 20; $i++) {
+            $bowlingGame->play(1);
+        }
+        $this->assertEquals(20, $bowlingGame->score());
     }
+
+    /** @test */
+    public function it_awards_one_roll_bonus_for_every_spare()
+    {
+        $bowlingGame = new BowlingGame;
+        $bowlingGame->play(4);
+        $bowlingGame->play(6);
+
+
+        $bowlingGame->play(6);
+        for ($i = 0; $i < 17; $i++) {
+            $bowlingGame->play(0);
+        }
+        $this->assertEquals(22, $bowlingGame->score());
+    }
+
+    /** @test */
+    public function it_awards_two_roll_bonus_for_every_strike()
+    {
+        $bowlingGame = new BowlingGame;
+        $bowlingGame->play(10);
+        $bowlingGame->play(6);
+
+        $bowlingGame->play(6);
+        for ($i = 0; $i < 16; $i++) {
+            $bowlingGame->play(0);
+        }
+        $this->assertEquals(34, $bowlingGame->score());
+    }
+
+    /** @test */
+    public function a_spare_on_the_last_frame_grants_one_extra_ball()
+    {
+        $bowlingGame = new BowlingGame;
+
+        for ($i = 0; $i < 18; $i++) {
+            $bowlingGame->play(0);
+        }
+
+        $bowlingGame->play(5);
+        $bowlingGame->play(5);
+
+        $bowlingGame->play(5);
+
+        $this->assertEquals(15, $bowlingGame->score());
+    }
+    /** @test */
+    public function a_strike_on_the_last_frame_grants_two_extra_balls()
+    {
+        $bowlingGame = new BowlingGame;
+
+        for ($i = 0; $i < 18; $i++) {
+            $bowlingGame->play(0);
+        }
+
+        $bowlingGame->play(10);
+        $bowlingGame->play(10);
+
+        $bowlingGame->play(10);
+
+        $this->assertEquals(30, $bowlingGame->score());
+    }
+
+        /** @test */
+        public function it_scores_a_perfect_game()
+        {
+            $bowlingGame = new BowlingGame;
     
+            for ($i = 0; $i < 12; $i++) {
+                $bowlingGame->play(10);
+            }
+    
+            $this->assertEquals(300, $bowlingGame->score());
+        }
 }
